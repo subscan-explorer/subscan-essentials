ab96de3eb0527c1c8b1823e7907fd9186b0bb054
carlhong <acarlhong@gmail.com>
2020-09-08 17:20:46 +0800

    fix typo

--- ui/nuxt.config.js
+++ ui/nuxt.config.js
@@ -53,12 +53,12 @@ export default {
 
   axios: {
     proxy: process.env.NODE_ENV !== 'production',
-    browserBaseURL: process.env.NODE_ENV !== 'production' ? "" : "http://127.0.0.1/4399"
+    browserBaseURL: process.env.NODE_ENV !== 'production' ? "" : "http://127.0.0.1:4399"
   },
 
   proxy: {
     "/api": {
-      target: "http://127.0.0.1/4399", // 接口的域名
+      target: "http://127.0.0.1:4399", // 接口的域名
       secure: false,
       changeOrigin: true,
       pathRewrite: {
