a4bba5a9921427dbecc428f57cb821c03443cbba
Nathan Whitaker <nathan.whitaker01@gmail.com>
2023-05-10 13:16:06 -0700

    Add endpoint for era points

--- plugins/staking/dao/erainfo.go
+++ plugins/staking/dao/erainfo.go
@@ -3,6 +3,7 @@ package dao
 import (
 	"github.com/itering/subscan/plugins/staking/model"
 	"github.com/itering/subscan/plugins/storage"
+	"github.com/itering/subscan/util/address"
 )
 
 func NewEraInfo(db storage.DB, info *model.EraInfo) error {
@@ -18,4 +19,21 @@ func GetEraInfoList(db storage.DB, page, row int) ([]model.EraInfo, int) {
 		Order:        "era DESC",
 	})
 	return list, count
+}
+
+type EraAndPoints struct {
+	Era    uint32
+	Points map[address.SS58Address]uint32
+}
+
+func GetEraPointsList(db storage.DB, page, row int) ([]EraAndPoints, int) {
+	var list []model.EraInfo
+	db.Query(&model.EraInfo{}).Select("era, validator_points").Order("era DESC").Limit(row).Offset((page - 1) * row).Find(&list)
+	var result []EraAndPoints
+
+	for _, item := range list {
+		result = append(result, EraAndPoints{Era: item.Era, Points: item.ValidatorPoints.Data()})
+	}
+
+	return result, len(result)
 }
