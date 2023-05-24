1723513e87c7952f150f987f468f896e427ab86c
Nathan Whitaker <nathan.whitaker01@gmail.com>
2023-05-24 12:24:38 -0700

    Optimize sql types and indexes a bit

--- plugins/staking/model/model.go
+++ plugins/staking/model/model.go
@@ -8,35 +8,35 @@ import (
 
 type Payout struct {
 	ID             uint                `gorm:"primary_key" json:"-"`
-	Account        address.SS58Address `gorm:"index;default: null;size:100" json:"account"`
+	Account        address.SS58Address `gorm:"index;type:char(48);default: null;size:100" json:"account"`
 	Amount         decimal.Decimal     `gorm:"type:decimal(30,0);" json:"amount"`
 	Era            uint32              `gorm:"index" json:"era"`
-	Stash          address.SS58Address `gorm:"index;default: null;size:100" json:"stash"`
-	ValidatorStash address.SS58Address `gorm:"index" sql:"default: null;size:100" json:"validator_stash"`
+	Stash          address.SS58Address `gorm:"index;type:char(48);default: null;size:100" json:"stash"`
+	ValidatorStash address.SS58Address `gorm:"index;type:char(48);default: null;size:100" json:"validator_stash"`
 	BlockTimestamp uint64              `gorm:"index" json:"block_timestamp"`
-	ModuleId       string              `json:"module_id"`
-	EventId        string              `json:"event_id"`
+	ModuleId       string              `gorm:"type:varchar(50)" json:"module_id"`
+	EventId        string              `gorm:"type:varchar(50)" json:"event_id"`
 	SlashKton      bool                `json:"slash_kton"`
-	ExtrinsicIndex string              `json:"extrinsic_index"`
-	EventIndex     string              `json:"event_index"`
+	ExtrinsicIndex string              `gorm:"type:varchar(22)" json:"extrinsic_index"`
+	EventIndex     string              `gorm:"type:varchar(22)" json:"event_index"`
 	Claimed        bool                `gorm:"index" json:"-"`
 }
 
 type PoolPayout struct {
 	ID             uint                `gorm:"primary_key" json:"-"`
-	Account        address.SS58Address `gorm:"index;default: null;size:100" json:"account"`
+	Account        address.SS58Address `gorm:"index;type:char(48);default: null;size:100" json:"account"`
 	Amount         decimal.Decimal     `gorm:"type:decimal(30,0);" json:"amount"`
 	PoolId         uint32              `gorm:"index" json:"pool_id"`
-	ModuleId       string              `json:"module_id"`
-	EventId        string              `json:"event_id"`
-	ExtrinsicIndex string              `json:"extrinsic_index"`
-	EventIndex     string              `json:"event_index"`
-	BlockTimestamp uint64              `gorm:"index" json:"block_timestamp"`
+	ModuleId       string              `gorm:"type:varchar(50)" json:"module_id"`
+	EventId        string              `gorm:"type:varchar(50)" json:"event_id"`
+	ExtrinsicIndex string              `gorm:"type:varchar(22)" json:"extrinsic_index"`
+	EventIndex     string              `gorm:"type:varchar(22)" json:"event_index"`
+	BlockTimestamp uint64              `gorm:"index:,sort:desc" json:"block_timestamp"`
 }
 
 type ValidatorPrefs struct {
 	ID                uint                `gorm:"primary_key" json:"-"`
-	Account           address.SS58Address `gorm:"index;unique;default: null;size:100" json:"account"`
+	Account           address.SS58Address `gorm:"index;type:char(48);unique;default: null;size:100" json:"account"`
 	Commission        decimal.Decimal     `sql:"type:decimal(12,11);" json:"commission"`
 	BlockedNomination bool                `json:"blocked_nomination"`
 	Era               uint32              `gorm:"index"`
